f6e7dc5d3fa3c99de409ce4797a18f370aefefad
OddoAkbar <45064803+OddoAkbar@users.noreply.github.com>
2021-03-13 13:39:30 -0300

    First Commit!

---
+++ src/pages/Discover/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Heading from '../../components/Heading';
+
+import { Wrapper } from './styles';
+
+const Discover: React.FC = () => {
+  return (
+    <Wrapper>
+      <Heading>Descubra</Heading>
+    </Wrapper>
+  );
+};
+
+export default Discover;
